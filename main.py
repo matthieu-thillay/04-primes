a5bf7c1a6f9106b1e52995bdea6f2fe0fbe6ae3c
Matthieu <matthieu.thillay@edu.esiee.fr>
2025-09-19 08:15:32 +0000

    premiere soumission

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
-from math import sqrt
 
 #### Fonction secondaire
 
 
+"""
+Module principal pour l'affichage des nombres premiers inférieurs à 100.
+"""
 def isprime(p):
 
-    # votre code ici
-
-    pass
+    """
+    Détermine si p est un nombre premier.
+    >>> isprime(0)
+    False
+    >>> isprime(1)
+    False
+    >>> isprime(2)
+    True
+    >>> isprime(3)
+    True
+    >>> isprime(4)
+    False
+    >>> isprime(5)
+    True
+    """
+    if p < 2:
+        return False
+    for i in range(2, int(p ** 0.5) + 1):
+        if p % i == 0:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
 
-    # vos appels à la fonction secondaire ici
-
+    """Affiche les nombres premiers inférieurs à 100."""
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
-
     print()
 
 
